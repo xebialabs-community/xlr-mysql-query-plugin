f0dc98741a575639c18fa2d6d5969c65a412568d
Randy Mazin <randy.mazin@gmail.com>
2017-04-29 23:07:04 -0400

    Update run_queries.py

--- src/main/resources/mysql/run_queries.py
+++ src/main/resources/mysql/run_queries.py
@@ -8,4 +8,3 @@ for q in queries:
     db1 = os.system(mysqlServer['mysqlHome'] + "mysql -h " + mysqlServer['url'] + " -D " + mysqlServer['schema'] + " -u " + mysqlServer['username'] + " -p" + mysqlServer['password'] + " -A -e '" + q.replace("\n" ,"").replace("'", "\'") + "'")
     os.system("exit")  # close client
     print 'Successfully ran the query: ',  q
-    #print mysqlServer['mysqlHome'] + "mysql -h " + mysqlServer['url'] + " -D " + mysqlServer['schema'] + " -u " + mysqlServer['username'] + " -p" + mysqlServer['password'] + " -A -e '" + q.replace("\n" ,"").replace("'", "\'") + "'"
